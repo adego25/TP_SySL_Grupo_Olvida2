b580e02a6f2efb30a1614535c76146471e3038bb
adego25 <adegoumoispasserini@frba.utn.edu.ar>
2025-09-29 22:14:18 -0300

    1b

--- scripts/ejercicio_1a.sh
+++ scripts/ejercicio_1a.sh
@@ -1,1 +1,2 @@
 sed 's/\./\.\n/g' ../consignas/breve_historia.txt > ../evidencia/breve_historia_1a.txt
+echo "Archivo breve_historia_1a.txt creado en la carpeta evidencia"
---
+++ scripts/ejercicio_1b.sh
@@ -0,0 +1,2 @@
+sed '/^[[:space:]]*$/d' breve_historia.txt > breve_historia_1b.txt
+echo "Archivo breve_historia_1b.txt creado en la carpeta evidencia"
