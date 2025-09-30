95b6ec4ccb1ccad097a4dc5bda6312b9b35c72ac
adego25 <adegoumoispasserini@frba.utn.edu.ar>
2025-09-29 22:17:39 -0300

    ejercicio 1b corregido y evidencia

--- scripts/ejercicio_1b.sh
+++ scripts/ejercicio_1b.sh
@@ -1,2 +1,2 @@
-sed '/^[[:space:]]*$/d' breve_historia.txt > breve_historia_1b.txt
+sed '/^[[:space:]]*$/d' ../consignas/breve_historia.txt > ../evidencia/breve_historia_1b.txt
 echo "Archivo breve_historia_1b.txt creado en la carpeta evidencia"
